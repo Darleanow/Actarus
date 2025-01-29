7b0e775b1a9d8e1525ebbf387eba1ae1e1768a23
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-29 18:41:33 +0100

    feat(Bootloader): Enable protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0 ; origin
+ORG 0x7c00 ; origin
 BITS 16 ; 16 bit architecture (for now)
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start: ; Bios Parameter Block specific
     jmp short start
@@ -8,58 +11,68 @@ _start: ; Bios Parameter Block specific
 times 33 db 0 ; fill null to BPB (offset total is 33)
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli ; clear interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-
-    mov ax, 0x00 ; set stack 0
     mov ss, ax
     mov sp, 0x7c00 ; stack pointer
     sti ; enable interrupts
 
-    mov ah, 2 ; Read sector cmd
-    mov al, 1 ; one sector to read
-    mov ch, 0 ; cylinder low eight bits
-    mov cl, 2 ; sector 2
-    mov dh, 0 ; head number
 
-    mov bx, buffer ; set data to our buffer
-    int 0x13
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    jc error
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-    mov si, buffer
-    call print
+; offset 0x8
+gdt_code:     ; CH points to this
+    dw 0xffff ; segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; access byte
+    db 11001111b ; High 4 bit & low 4 bit flags
+    db 0      ; base 24-31 bits
+
+; offset 0x10
+gdt_data:     ; DS, SS, ES, FS, GS
+    dw 0xffff ; segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; access byte
+    db 11001111b ; High 4 bit & low 4 bit flags
+    db 0      ; base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $ ; endless loop
-
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10 ; BIOS call
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510-($- $$) db 0 ; fill at least 510 bytes of data, pabbing rest to 0
 dw 0xAA55 ; signature
-
-buffer:
