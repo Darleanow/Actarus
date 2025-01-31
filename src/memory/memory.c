6cd24dc12d089b28d76a11ca7427423c303f24c3
shyybi <charlotte.rod@outlook.com>
2025-01-31 18:36:28 +0100

    norme : memory.c to the 42 norme

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,11 +1,16 @@
 #include "memory.h"
 
-void* memset(void* pointer, int c, size_t size)
+void	*memset(void *pointer, int c, size_t size)
 {
-    char* c_pointer = (char*) pointer;
-    for (int i =0; i < size; i++)
-    {
-        c_pointer[i] = (char) c;
-    }
-    return pointer;
+	char	*c_pointer;
+	int		i;
+
+	*c_pointer = (char *) pointer;
+	i = 0;
+	while (i < size)
+	{
+		c_pointer[i] = (char) c;
+		i++;
+	}
+	return (pointer);
 }
