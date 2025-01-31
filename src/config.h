e673c0ad27d280259b472e130b8bf7192d901d7a
Enzo Hugonnier <enzohugonn@gmail.com>
2025-01-31 17:48:26 +0100

    feat(Kernel): Implement HEAP

--- src/config.h
+++ src/config.h
@@ -5,5 +5,10 @@
 #define KERNEL_DATA_SELECTOR 0x10
 #define ACTARUS_TOTAL_INTERRUPTS 512
 
-#endif
+// TODO(Darleanow): Determine installed ram to auto adapt heap size
+#define ACTARUS_HEAP_SIZE_BYTES 104857600 // 100MB
+#define ACTARUS_HEAP_BLOCK_SIZE 4096
+#define ACTARUS_HEAP_ADDRESS 0x01000000
+#define ACTARUS_HEAP_TABLE_ADDRESS 0x00007E00
 
+#endif
