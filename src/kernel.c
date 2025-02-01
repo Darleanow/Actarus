0644381f85edfcb49e0c68346ff521a918370766
Enzo Hugonnier <enzohugonn@gmail.com>
2025-02-01 23:05:36 +0100

    feat(Disk): Implement disk stream

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "string/string.h"
 #include "disk/disk.h"
 #include "fs/pparser.h"
+#include "disk/streamer.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -95,10 +96,10 @@ void kernel_main()
     // Enable sys interrupts
     enable_interrupts();
 
-    struct path_root* root_path = pathparser_parse("0:/bin/shell.exe", NULL);
+    struct disk_stream *stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
 
-    if (root_path)
-    {
-
-    }
+    while (1) {}
 }
