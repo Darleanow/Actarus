dd68315bf121d989b7b40d8775c30210a0157698
Enzo Hugonnier <enzohugonn@gmail.com>
2025-02-09 13:09:56 +0100

    feat(Commands): Add first kcommand

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -63,7 +63,7 @@ void idt_init()
 
 void isr80h_register_command(int command_id, ISR80H_COMMAND command)
 {
-    if (command_id <= 0 || command_id >= ACTARUS_MAX_ISR80H_COMMANDS)
+    if (command_id < 0 || command_id >= ACTARUS_MAX_ISR80H_COMMANDS)
     {
         panic("Command is out of bounds");
     }
@@ -80,7 +80,7 @@ void *isr80h_handle_command(int command, struct interrupt_frame *frame)
 {
     void *result = 0;
 
-    if (command <= 0 || command >= ACTARUS_MAX_ISR80H_COMMANDS)
+    if (command < 0 || command >= ACTARUS_MAX_ISR80H_COMMANDS)
     {
         // Invalid command
         return 0;
